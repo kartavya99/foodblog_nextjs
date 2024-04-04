f7e26e33c7c549cdf9e663288946c8e6e2110ea4
kartavya <kartavyas@gmail.com>
2024-04-04 17:44:56 +1100

    error and notfound page added

---
+++ app/meals/error.js
@@ -0,0 +1,11 @@
+"use client";
+
+const Error = ({ error }) => {
+  return (
+    <main className="error">
+      <h1>An error occurred</h1>
+      <p>Failed to fetch meal data please try again</p>
+    </main>
+  );
+};
+export default Error;
